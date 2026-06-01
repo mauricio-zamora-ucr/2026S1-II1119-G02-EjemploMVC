beb1c7bf3bcd57f41d4e8f3a5cbd81491aa88edc
Mauricio Andrés Zamora Hernández <64853106+mauricio-zamora-ucr@users.noreply.github.com>
2026-06-01 15:12:17 +0000

    x

--- ejemplo02.py
+++ ejemplo02.py
@@ -2,6 +2,14 @@ from eii_utils import mostrar_menu
 
 lista_nombres:list[str] = ['Diego ',' Jimena','santiago','Giuliana','ALISSA','Arath','Abigail', 'Lizeth', 'Isabella', 'Paula']
 
+lista_estudiante:list[dict[str, str]] = []
+e1 = {'nombre':'Jimena', 'carne':'a', 'lugar':'Cartago'}
+lista_estudiante.append(e1)
+e2 = {'nomdre':'Ariadna', 'carne':'b', 'lugar':'Heredia'}
+lista_estudiante.append(e2)
+e3 = {'nombre':'Arath', 'carne':'c', 'lugar':'San Jose'}
+lista_estudiante.append(e3)
+
 def selecionar_estudiante(lista:list[str]) -> str:
     lista_procesada = [  i.strip().capitalize() for i in lista  ]
     lista_procesada.sort()
@@ -11,4 +19,5 @@ def selecionar_estudiante(lista:list[str]) -> str:
     return lista_procesada[opcion-1]
 
 if __name__ == '__main__':
-    print(selecionar_estudiante(lista_nombres))
+    #print(selecionar_estudiante(lista_nombres))
+    print(lista_estudiante)
