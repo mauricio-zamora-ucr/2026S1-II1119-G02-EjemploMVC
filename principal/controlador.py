a9bde75ea522e4c10ea5f2064922cc3180b4a498
Mauricio Andrés Zamora Hernández <64853106+mauricio-zamora-ucr@users.noreply.github.com>
2026-06-01 15:50:53 +0000

    x

---
+++ principal/controlador.py
@@ -0,0 +1,20 @@
+from principal.vista import mostrar_menu_principal
+from eii_utils import limpiar_consola, pausar
+
+def iniciar_aplicacion() -> None:
+    opcion:int = -1
+
+    while opcion != 0:
+        limpiar_consola()
+        opcion = mostrar_menu_principal()
+        match opcion:
+            case 1:
+                print('Ver producto')
+                pausar()
+            case 2:
+                print('ver clientes')
+                pausar
+            case 0:
+                print('Hasta la vista, Baby!')
+                pausar()
+
