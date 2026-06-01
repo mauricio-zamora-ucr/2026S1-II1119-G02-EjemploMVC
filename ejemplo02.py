3c311cf50c2ddd4215d759baf1251cfeabdeb61c
Mauricio Andrés Zamora Hernández <64853106+mauricio-zamora-ucr@users.noreply.github.com>
2026-06-01 15:02:14 +0000

    x

--- ejemplo02.py
+++ ejemplo02.py
@@ -3,4 +3,10 @@ from eii_utils import mostrar_menu
 lista_nombres:list[str] = ['Diego ',' Jimena','santiago','Giuliana','ALISSA','Arath','Abigail', 'Lizeth', 'Isabella', 'Paula']
 
 def selecionar_estudiante(lista:list[str]) -> str:
-    lista_procesada
+    lista_procesada = [  i.strip().capitalize() for i in lista  ]
+    lista_procesada.sort()
+    print(lista_procesada)
+    return ''
+
+if __name__ == '__main__':
+    selecionar_estudiante(lista_nombres)
