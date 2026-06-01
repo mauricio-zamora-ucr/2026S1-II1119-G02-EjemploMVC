d962cf9f906a79c0e5ca2b15889707a7ae913a3d
Mauricio Andrés Zamora Hernández <64853106+mauricio-zamora-ucr@users.noreply.github.com>
2026-06-01 14:58:13 +0000

    x

---
+++ ejemplo02.py
@@ -0,0 +1,6 @@
+from eii_utils import mostrar_menu
+
+lista_nombres:list[str] = ['Diego ',' Jimena','santiago','Giuliana','ALISSA','Arath','Abigail', 'Lizeth', 'Isabella', 'Paula']
+
+def selecionar_estudiante(lista:list[str]) -> str:
+    lista_procesada
