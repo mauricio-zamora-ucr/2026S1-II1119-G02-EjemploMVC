2166ec5923e1f6bd146c3108761168393449fa2b
Mauricio Andrés Zamora Hernández <64853106+mauricio-zamora-ucr@users.noreply.github.com>
2026-06-01 14:31:43 +0000

    sincro

--- ejemplos.py
+++ ejemplos.py
@@ -11,3 +11,12 @@ for nombre in lista_nombres:
     lista_largos.append(len(nombre))
 
 print(lista_largos)
+
+lista_largo_nombre = list( zip(lista_largos, lista_nombres) )
+print(lista_largo_nombre)
+
+def extrae_largo(x):
+    return x[0]
+
+for x in lista_largo_nombre:
+    print(extrae_largo(x))
