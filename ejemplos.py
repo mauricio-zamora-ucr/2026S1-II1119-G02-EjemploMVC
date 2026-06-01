44b4467e2bded4f7749d171b47118c2e74503774
Mauricio Andrés Zamora Hernández <64853106+mauricio-zamora-ucr@users.noreply.github.com>
2026-06-01 14:49:29 +0000

    x

--- ejemplos.py
+++ ejemplos.py
@@ -12,11 +12,23 @@ for nombre in lista_nombres:
 
 print(lista_largos)
 
-lista_largo_nombre = list( zip(lista_largos, lista_nombres) )
+lista_largo_nombre = list( zip(lista_nombres,lista_largos) )
 print(lista_largo_nombre)
 
 def extrae_largo(x):
-    return x[0]
+    return x[1]
 
-for x in lista_largo_nombre:
-    print(extrae_largo(x))
+#lista_largo_nombre.sort(key=extrae_largo)
+
+lista_largo_nombre.sort(key=lambda x: x[1])
+
+largo = max(lista_largos)
+print(largo)
+
+print(lista_largo_nombre)
+
+def filtrar_largo(tupla):
+    return extrae_largo(tupla) == 8
+
+los_mas_largos = list(filter(filtrar_largo, lista_largo_nombre))
+print(los_mas_largos)
